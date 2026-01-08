957f4e63c91be999bf9ff9f65ec762faee0a1ee2
Pablo Marinho <pablolucas-1@outlook.com>
2026-01-08 08:27:05 -0300

    iniciada a criação do sidebar e dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+  <x-side-bar />
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-  Route::redirect('/', 'home');
-  Route::view('/home', 'home')->name('home');
+  Route::redirect('/', 'dashboard');
+  Route::view('/dashboard', 'dashboard')->name('dashboard');
 });
